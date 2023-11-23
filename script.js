0ee1dac7c9e461b75cbb660d6953d3984c280fc0
Dabhi Hitesh <132338545+dabhihitesh36@users.noreply.github.com>
2023-11-23 18:58:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const openmodal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closemodal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openmodal);
+
+btnCloseModal.addEventListener('click', closemodal);
+overlay.addEventListener('click', closemodal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closemodal();
+  }
+});
